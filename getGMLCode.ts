dec8bcf89bc1fd577937148067a40e4863421c3a
QuentinJanuel <facemashldv@laposte.net>
2020-01-13 15:39:27 +0100

    Fixed GM80 font bug and variable conflicts

--- getGMLCode.ts
+++ getGMLCode.ts
@@ -13,7 +13,7 @@ export class GMLCode {
 			__ONLINE_n = buffer_read_uint16(__ONLINE_buffer);
 			for(__ONLINE_i = 0; __ONLINE_i < n; __ONLINE_i += 1){
 				__ONLINE_oPlayer = instance_create(0, 0, __ONLINE_onlinePlayer);
-				__ONLINE_oPlayer.ID = buffer_read_string(__ONLINE_buffer);
+				__ONLINE_oPlayer.__ONLINE_ID = buffer_read_string(__ONLINE_buffer);
 				__ONLINE_oPlayer.x = buffer_read_int32(__ONLINE_buffer);
 				__ONLINE_oPlayer.y = buffer_read_int32(__ONLINE_buffer);
 				__ONLINE_oPlayer.sprite_index = buffer_read_int32(__ONLINE_buffer);
@@ -21,8 +21,8 @@ export class GMLCode {
 				__ONLINE_oPlayer.image_xscale = buffer_read_float32(__ONLINE_buffer);
 				__ONLINE_oPlayer.image_yscale = buffer_read_float32(__ONLINE_buffer);
 				__ONLINE_oPlayer.image_angle = buffer_read_float32(__ONLINE_buffer);
-				__ONLINE_oPlayer.oRoom = buffer_read_uint16(__ONLINE_buffer);
-				__ONLINE_oPlayer.name = buffer_read_string(__ONLINE_buffer);
+				__ONLINE_oPlayer.__ONLINE_oRoom = buffer_read_uint16(__ONLINE_buffer);
+				__ONLINE_oPlayer.__ONLINE_name = buffer_read_string(__ONLINE_buffer);
 			}
 		}else{
 			__ONLINE_socket = socket_create();
@@ -39,7 +39,7 @@ export class GMLCode {
 			buffer_write_uint8(__ONLINE_buffer, 3);
 			buffer_write_string(__ONLINE_buffer, __ONLINE_name);
 			buffer_write_string(__ONLINE_buffer, "${ID}");
-			buffer_write_string(__ONLINE_buffer, "${path.basename(gamePath, "exe")}");
+			buffer_write_string(__ONLINE_buffer, "${path.basename(gamePath, ".exe")}");
 			socket_write_message(__ONLINE_socket, __ONLINE_buffer);
 		}
 		__ONLINE_pExists = false;
@@ -64,14 +64,14 @@ export class GMLCode {
 					__ONLINE_ID = buffer_read_string(__ONLINE_buffer);
 					__ONLINE_found = false;
 					for(__ONLINE_i = 0; __ONLINE_i < instance_number(__ONLINE_onlinePlayer); __ONLINE_i += 1){
-						if(instance_find(__ONLINE_onlinePlayer, __ONLINE_i).ID == __ONLINE_ID){
+						if(instance_find(__ONLINE_onlinePlayer, __ONLINE_i).__ONLINE_ID == __ONLINE_ID){
 							__ONLINE_found = true;
 							break;
 						}
 					}
 					if(!__ONLINE_found){
 						__ONLINE_oPlayer = instance_create(0, 0, __ONLINE_onlinePlayer);
-						__ONLINE_oPlayer.ID = __ONLINE_ID;
+						__ONLINE_oPlayer.__ONLINE_ID = __ONLINE_ID;
 					}
 					break;
 				case 1:
@@ -79,7 +79,7 @@ export class GMLCode {
 					__ONLINE_ID = buffer_read_string(__ONLINE_buffer);
 					for(__ONLINE_i = 0; __ONLINE_i < instance_number(__ONLINE_onlinePlayer); __ONLINE_i += 1){
 						__ONLINE_oPlayer = instance_find(__ONLINE_onlinePlayer, __ONLINE_i);
-						if(__ONLINE_oPlayer.ID == __ONLINE_ID){
+						if(__ONLINE_oPlayer.__ONLINE_ID == __ONLINE_ID){
 							with(__ONLINE_oPlayer){
 								instance_destroy();
 							}
@@ -94,7 +94,7 @@ export class GMLCode {
 					__ONLINE_oPlayer = 0;
 					for(__ONLINE_i = 0; __ONLINE_i < instance_number(__ONLINE_onlinePlayer); __ONLINE_i += 1){
 						__ONLINE_oPlayer = instance_find(__ONLINE_onlinePlayer, __ONLINE_i);
-						if(__ONLINE_oPlayer.ID == __ONLINE_ID){
+						if(__ONLINE_oPlayer.__ONLINE_ID == __ONLINE_ID){
 							__ONLINE_found = true;
 							break;
 						}
@@ -102,7 +102,7 @@ export class GMLCode {
 					if(!__ONLINE_found){
 						__ONLINE_oPlayer = instance_create(0, 0, __ONLINE_onlinePlayer);
 					}
-					__ONLINE_oPlayer.ID = __ONLINE_ID;
+					__ONLINE_oPlayer.__ONLINE_ID = __ONLINE_ID;
 					__ONLINE_oPlayer.x = buffer_read_int32(__ONLINE_buffer);
 					__ONLINE_oPlayer.y = buffer_read_int32(__ONLINE_buffer);
 					__ONLINE_oPlayer.sprite_index = buffer_read_int32(__ONLINE_buffer);
@@ -110,8 +110,8 @@ export class GMLCode {
 					__ONLINE_oPlayer.image_xscale = buffer_read_float32(__ONLINE_buffer);
 					__ONLINE_oPlayer.image_yscale = buffer_read_float32(__ONLINE_buffer);
 					__ONLINE_oPlayer.image_angle = buffer_read_float32(__ONLINE_buffer);
-					__ONLINE_oPlayer.oRoom = buffer_read_uint16(__ONLINE_buffer);
-					__ONLINE_oPlayer.name = buffer_read_string(__ONLINE_buffer);
+					__ONLINE_oPlayer.__ONLINE_oRoom = buffer_read_uint16(__ONLINE_buffer);
+					__ONLINE_oPlayer.__ONLINE_name = buffer_read_string(__ONLINE_buffer);
 					break;
 				case 4:
 					// CHAT MESSAGE
@@ -120,7 +120,7 @@ export class GMLCode {
 					__ONLINE_oPlayer = 0;
 					for(__ONLINE_i = 0; __ONLINE_i < instance_number(__ONLINE_onlinePlayer); __ONLINE_i += 1){
 						__ONLINE_oPlayer = instance_find(__ONLINE_onlinePlayer, i);
-						if(__ONLINE_oPlayer.ID == __ONLINE_ID){
+						if(__ONLINE_oPlayer.__ONLINE_ID == __ONLINE_ID){
 							__ONLINE_found = true;
 							break;
 						}
@@ -128,8 +128,8 @@ export class GMLCode {
 					if(__ONLINE_found){
 						__ONLINE_message = buffer_read_string(__ONLINE_buffer);
 						__ONLINE_oChatbox = instance_create(0, 0, __ONLINE_chatbox);
-						__ONLINE_oChatbox.message = __ONLINE_message;
-						__ONLINE_oChatbox.follower = __ONLINE_oPlayer;
+						__ONLINE_oChatbox.__ONLINE_message = __ONLINE_message;
+						__ONLINE_oChatbox.__ONLINE_follower = __ONLINE_oPlayer;
 						if(__ONLINE_oPlayer.visible){
 							sound_play(__ONLINE_sndChatbox);
 						}
@@ -143,7 +143,7 @@ export class GMLCode {
 					__ONLINE_sY = buffer_read_float64(__ONLINE_buffer);
 					__ONLINE_sRoom = buffer_read_int16(__ONLINE_buffer);
 					__ONLINE_a = instance_create(0, 0, __ONLINE_playerSaved);
-					__ONLINE_a.name = __ONLINE_sName;
+					__ONLINE_a.__ONLINE_name = __ONLINE_sName;
 					buffer_clear(__ONLINE_buffer);
 					buffer_write_uint8(__ONLINE_buffer, __ONLINE_sGravity);
 					buffer_write_int32(__ONLINE_buffer, __ONLINE_sX);
@@ -234,8 +234,8 @@ export class GMLCode {
 					buffer_write_string(__ONLINE_buffer, __ONLINE_message);
 					socket_write_message(__ONLINE_socket, __ONLINE_buffer);
 					__ONLINE_oChatbox = instance_create(0, 0, __ONLINE_chatbox);
-					__ONLINE_oChatbox.message = __ONLINE_message;
-					__ONLINE_oChatbox.follower = __ONLINE_p;
+					__ONLINE_oChatbox.__ONLINE_message = __ONLINE_message;
+					__ONLINE_oChatbox.__ONLINE_follower = __ONLINE_p;
 					sound_play(__ONLINE_sndChatbox);
 				}
 			}
@@ -274,26 +274,26 @@ export class GMLCode {
 	public static getOnlinePlayerCreate = function(): string {
 		return `
 		/// ONLINE
-		alpha = 1;
+		__ONLINE_alpha = 1;
 		`;
 	}
 	public static getOnlinePlayerEndStep = function(player: GMObject, player2: GMObject): string {
 		return `
 		/// ONLINE
-		visible = oRoom == room;
-		image_alpha = alpha;
-		p = ${player.name};
+		visible = __ONLINE_oRoom == room;
+		image_alpha = __ONLINE_alpha;
+		__ONLINE_p = ${player.name};
 		${
 			player2 !== undefined ?
 			`
-			if(!instance_exists(p)){
-				p = ${player2.name};
+			if(!instance_exists(__ONLINE_p)){
+				__ONLINE_p = ${player2.name};
 			}
 			` : ""
 		}
-		if(instance_exists(p)){
-			dist = distance_to_object(${player.name});
-			image_alpha = min(alpha, dist/100);
+		if(instance_exists(__ONLINE_p)){
+			__ONLINE_dist = distance_to_object(${player.name});
+			image_alpha = min(__ONLINE_alpha, __ONLINE_dist/100);
 		}
 		`;
 	}
@@ -301,120 +301,120 @@ export class GMLCode {
 		return `
 		/// ONLINE
 		draw_sprite_ext(sprite_index, image_index, x, y, image_xscale, image_yscale, image_angle, c_white, image_alpha);
-		_alpha = draw_get_alpha();
-		_color = draw_get_color();
+		__ONLINE__alpha = draw_get_alpha();
+		__ONLINE__color = draw_get_color();
 		draw_set_alpha(image_alpha);
 		draw_set_font(__ONLINE_ftOnlinePlayerName);
 		draw_set_valign(fa_center);
 		draw_set_halign(fa_center);
 		draw_set_color(c_black);
-		border = 2;
-		padding = 30;
-		xx = x;
-		yy = y-padding;
+		__ONLINE_border = 2;
+		__ONLINE_padding = 30;
+		__ONLINE_xx = x;
+		__ONLINE_yy = y-__ONLINE_padding;
 		draw_set_alpha(1);
-		draw_text(xx+border, yy, name);
-		draw_text(xx, yy+border, name);
-		draw_text(xx-border, yy, name);
-		draw_text(xx, yy-border, name);
+		draw_text(__ONLINE_xx+__ONLINE_border, __ONLINE_yy, __ONLINE_name);
+		draw_text(__ONLINE_xx, __ONLINE_yy+__ONLINE_border, __ONLINE_name);
+		draw_text(__ONLINE_xx-__ONLINE_border, __ONLINE_yy, __ONLINE_name);
+		draw_text(__ONLINE_xx, __ONLINE_yy-__ONLINE_border, __ONLINE_name);
 		draw_set_color(c_white);
-		draw_text(xx, yy, name);
-		draw_set_alpha(_alpha);
-		draw_set_color(_color);
+		draw_text(__ONLINE_xx, __ONLINE_yy, __ONLINE_name);
+		draw_set_alpha(__ONLINE__alpha);
+		draw_set_color(__ONLINE__color);
 		`;
 	}
 	public static getChatboxCreate = function(): string {
 		return `
 		/// ONLINE
-		paddingText = 10;
-		width = 250;
-		padding = 15;
-		alpha = 1;
-		fade = false;
-		fadeAlpha = 1;
-		t = 70;
-		sep = -1;
-		maxTextWidth = width-2*paddingText;
+		__ONLINE_paddingText = 10;
+		__ONLINE_width = 250;
+		__ONLINE_padding = 15;
+		__ONLINE_alpha = 1;
+		__ONLINE_fade = false;
+		__ONLINE_fadeAlpha = 1;
+		__ONLINE_t = 70;
+		__ONLINE_sep = -1;
+		__ONLINE_maxTextWidth = __ONLINE_width-2*__ONLINE_paddingText;
 		`;
 	}
 	public static getChatboxEndStep = function(player: GMObject, player2: GMObject): string {
 		return `
 		/// ONLINE
-		f = follower;
-		if(f == ${player.name} && !instance_exists(f)){
+		__ONLINE_f = __ONLINE_follower;
+		if(__ONLINE_f == ${player.name} && !instance_exists(__ONLINE_f)){
 			${
 				player2 !== undefined ?
 				`
-				f = ${player2.name};
+				__ONLINE_f = ${player2.name};
 				` : ""
 			}
 		}
-		if(instance_exists(f)){
-			x = f.x;
-			y = f.y;
+		if(instance_exists(__ONLINE_f)){
+			x = __ONLINE_f.x;
+			y = __ONLINE_f.y;
 		}else{
 			instance_destroy();
 		}
-		if(fade){
-			fadeAlpha -= .02;
-			if(fadeAlpha <= 0){
+		if(__ONLINE_fade){
+			__ONLINE_fadeAlpha -= .02;
+			if(__ONLINE_fadeAlpha <= 0){
 				instance_destroy();
 			}
 		}
-		alpha = 1;
-		if(follower != player){
-			visible = follower.visible;
-			p = ${player.name};
+		__ONLINE_alpha = 1;
+		if(__ONLINE_follower != ${player.name}){
+			__ONLINE_visible = follower.visible;
+			__ONLINE_p = ${player.name};
 			${
 				player2 !== undefined ?
 				`
-				if(!instance_exists(p)){
-					p = ${player2.name};
+				if(!instance_exists(__ONLINE_p)){
+					__ONLINE_p = ${player2.name};
 				}
 				` : ""
 			}
-			if(instance_exists(p)){
-				dist = distance_to_object(${player.name});
-				alpha = dist/100;
+			if(instance_exists(__ONLINE_p)){
+				__ONLINE_dist = distance_to_object(${player.name});
+				__ONLINE_alpha = __ONLINE_dist/100;
 			}
 		}
-		t -= 1;
-		if(t < 0){
-			fade = true;
+		__ONLINE_t -= 1;
+		if(__ONLINE_t < 0){
+			__ONLINE_fade = true;
 		}
 		`;
 	}
 	public static getChatboxDraw = function(): string {
 		return `
 		/// ONLINE
-		textHeight = string_height_ext(message, sep, maxTextWidth);
-		height = textHeight+2*paddingText;
-		yOffset = -height/2+60;
-		left = 0;
-		right = room_width;
-		top = 0;
-		bottom = room_height;
+		__ONLINE_textHeight = string_height_ext(__ONLINE_message, __ONLINE_sep, __ONLINE_maxTextWidth);
+		__ONLINE_height = __ONLINE_textHeight+2*__ONLINE_paddingText;
+		__ONLINE_yOffset = -__ONLINE_height/2+60;
+		__ONLINE_left = 0;
+		__ONLINE_right = room_width;
+		__ONLINE_top = 0;
+		__ONLINE_bottom = room_height;
 		if(view_enabled && view_visible[0]){
-			left = view_xview[0];
-			right = left+view_wview[0];
-			top = view_yview[0];
-			bottom = top+view_hview[0];
+			__ONLINE_left = view_xview[0];
+			__ONLINE_right = left+view_wview[0];
+			__ONLINE_top = view_yview[0];
+			__ONLINE_bottom = top+view_hview[0];
 		}
-		xx = min(max(x, left+width/2+padding), right-width/2-padding);
-		yy = min(max(y-yOffset, top+height/2+padding), bottom-height/2-padding);
-		_alpha = draw_get_alpha();
-		_color = draw_get_color();
-		draw_set_alpha(min(alpha, fadeAlpha));
+		__ONLINE_xx = min(max(x, __ONLINE_left+__ONLINE_width/2+padding), __ONLINE_right-__ONLINE_width/2-__ONLINE_padding);
+		__ONLINE_yy = min(max(y-__ONLINE_yOffset, __ONLINE_top+__ONLINE_height/2+__ONLINE_padding), __ONLINE_bottom-__ONLINE_height/2-__ONLINE_padding);
+		__ONLINE__alpha = draw_get_alpha();
+		__ONLINE__color = draw_get_color();
+		draw_set_alpha(min(__ONLINE_alpha, __ONLINE_fadeAlpha));
 		draw_set_color(c_white);
-		draw_rectangle(xx-width/2, yy-height/2, xx+width/2, yy+height/2, false);
+		draw_rectangle(__ONLINE_xx-__ONLINE_width/2, __ONLINE_yy-__ONLINE_height/2, __ONLINE_xx+__ONLINE_width/2, __ONLINE_yy+__ONLINE_height/2, false);
 		draw_set_color(c_black);
-		draw_rectangle(xx-width/2, yy-height/2, xx+width/2, yy+height/2, true);
+		draw_rectangle(__ONLINE_xx-__ONLINE_width/2, __ONLINE_yy-__ONLINE_height/2, __ONLINE_xx+__ONLINE_width/2, __ONLINE_yy+__ONLINE_height/2, true);
 		draw_set_font(__ONLINE_ftOnlinePlayerName);
 		draw_set_valign(fa_center);
 		draw_set_halign(fa_center);
-		draw_text_ext(xx, yy, message, sep, maxTextWidth);
-		draw_set_alpha(_alpha);
-		draw_set_color(_color);
+		draw_text_ext(__ONLINE_xx, __ONLINE_yy, __ONLINE_message, __ONLINE_sep, __ONLINE_maxTextWidth);
+		draw_set_alpha(__ONLINE__alpha);
+		draw_set_color(__ONLINE__color);
 		`;
 	}
 	public static getPlayerSavedEndStep = function(): string {
@@ -429,28 +429,28 @@ export class GMLCode {
 	public static getPlayerSavedDraw = function(): string {
 		return `
 		/// ONLINE
-		xx = 20;
-		yy = 20;
+		__ONLINE_xx = 20;
+		__ONLINE_yy = 20;
 		if(view_enabled && view_visible[0]){
-			xx += view_xview[0];
-			yy += view_yview[0];
+			__ONLINE_xx += view_xview[0];
+			__ONLINE_yy += view_yview[0];
 		}
-		text = name+" saved!";
-		_alpha = draw_get_alpha();
-		_color = draw_get_color();
+		__ONLINE_text = __ONLINE_name+" saved!";
+		__ONLINE__alpha = draw_get_alpha();
+		__ONLINE__color = draw_get_color();
 		draw_set_valign(fa_top);
 		draw_set_halign(fa_left);
 		draw_set_alpha(image_alpha);
 		draw_set_font(__ONLINE_ftOnlinePlayerName);
 		draw_set_color(c_black);
-		draw_text(xx+1, yy, text);
-		draw_text(xx, yy+1, text);
-		draw_text(xx-1, yy, text);
-		draw_text(xx, yy-1, text);
+		draw_text(__ONLINE_xx+1, __ONLINE_yy, __ONLINE_text);
+		draw_text(__ONLINE_xx, __ONLINE_yy+1, __ONLINE_text);
+		draw_text(__ONLINE_xx-1, __ONLINE_yy, __ONLINE_text);
+		draw_text(__ONLINE_xx, __ONLINE_yy-1, __ONLINE_text);
 		draw_set_color(c_white);
-		draw_text(xx, yy, text);
-		draw_set_alpha(_alpha);
-		draw_set_color(_color);
+		draw_text(__ONLINE_xx, __ONLINE_yy, __ONLINE_text);
+		draw_set_alpha(__ONLINE__alpha);
+		draw_set_color(__ONLINE__color);
 		`;
 	}
 	public static getSaveGame = function(world: GMObject, player: GMObject, player2: GMObject): string {
@@ -490,7 +490,7 @@ export class GMLCode {
 			buffer_write_uint16(__ONLINE_buffer, __ONLINE_n);
 			for(__ONLINE_i = 0; __ONLINE_i < __ONLINE_n; __ONLINE_i += 1){
 				__ONLINE_oPlayer = instance_find(__ONLINE_onlinePlayer, __ONLINE_i);
-				buffer_write_string(__ONLINE_buffer, __ONLINE_oPlayer.ID);
+				buffer_write_string(__ONLINE_buffer, __ONLINE_oPlayer.__ONLINE_ID);
 				buffer_write_int32(__ONLINE_buffer, __ONLINE_oPlayer.x);
 				buffer_write_int32(__ONLINE_buffer, __ONLINE_oPlayer.y);
 				buffer_write_int32(__ONLINE_buffer, __ONLINE_oPlayer.sprite_index);
@@ -498,8 +498,8 @@ export class GMLCode {
 				buffer_write_float32(__ONLINE_buffer, __ONLINE_oPlayer.image_xscale);
 				buffer_write_float32(__ONLINE_buffer, __ONLINE_oPlayer.image_yscale);
 				buffer_write_float32(__ONLINE_buffer, __ONLINE_oPlayer.image_angle);
-				buffer_write_uint16(__ONLINE_buffer, __ONLINE_oPlayer.oRoom);
-				buffer_write_string(__ONLINE_buffer, __ONLINE_oPlayer.name);
+				buffer_write_uint16(__ONLINE_buffer, __ONLINE_oPlayer.__ONLINE_oRoom);
+				buffer_write_string(__ONLINE_buffer, __ONLINE_oPlayer.__ONLINE_name);
 			}
 			buffer_write_to_file(__ONLINE_buffer, "tempOnline");
 		}
